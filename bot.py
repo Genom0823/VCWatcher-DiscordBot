4b3bfe16aafe4d6936384f5d7006a30c2c4b85a7
mori-C <kilbara.h.morikawa@gmail.com>
2023-03-25 16:37:06 +0900

    update: function to show all channel's infomations

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
 # python3
+
+import discord
+
+import cogs.channel as ch
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for channel in ch.get_channelId(client):
+        print("----------")
+        print("チャンネル名：" + str(channel.name))
+        print("チャンネルID：" + str(channel.id))
+        print("----------")
+
+client.run(TOKEN)
