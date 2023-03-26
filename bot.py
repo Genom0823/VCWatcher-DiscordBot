68fa8bbb28d6a351941b28c8d87fa0162cf4fcd1
mori-C <kilbara.h.morikawa@gmail.com>
2023-03-27 01:53:49 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,10 +28,10 @@ async def on_voice_state_update(member, before, after):
         
         if after.channel != None:
 
-            text = f"{member.avatar}{member.name} が {after.channel}に入室しました"
+            text = f"{member.name} が {after.channel}に入室しました"
             notifyEmbed = discord.Embed( title="入室通知", color=0x00ff00, description=text )
             notifyEmbed.add_field( name = "日時", value = now )
-            notifyEmbed.set_author(name=member.name, icon_url=member.avatar)
+            notifyEmbed.set_thumbnail(url=member.avatar)
 
 
         elif after.channel == None:
